1195d2639d2fd3a2d49828b18826d656f3838887
piotr <piotr.swiecik@gmail.com>
2023-07-31 18:34:42 +0000

    fixed jenkins agent casc settings

--- jenkins_agent/agent.sh
+++ jenkins_agent/agent.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name jenkins_agent -p 2222:22 -e JENKINS_AGENT_SSH_PUBKEY=jenkins_agent.pub -v /var/run/docker.sock:/var/run/docker.sock jenkins-agent:latest
+docker run --name jenkins_agent -p 2222:22 -e JENKINS_AGENT_SSH_PUBKEY="$(cat key2.pub)" -v /var/run/docker.sock:/var/run/docker.sock jenkins-agent:latest
